bf422119f4e73d4b92d698819041c9480fd0acb4
jaimerodriguezbm <103222324+jaimerodriguezbm@users.noreply.github.com>
2022-04-07 20:05:53 -0500

    in progress

--- calculator2.js
+++ calculator2.js
@@ -11,6 +11,11 @@
 //Divide
 
 
+// Sqrt
+function sqrt(a) {
+    showResult('sqrt('+ a + ')',Math.sqrt(a));
+}
+
 //Helper functions
 function showResult(operation, result) {
     console.log(operation + ': ' + result)
